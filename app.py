0cc9127138cd679e1b24e7d2bc53aad8c840b6b6
Brett Kugler <bkugler@gdt-labs.local>
2018-06-22 11:03:33 -0500

    Manage OCP API key

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from os import environ as env
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World - from Crossvale on OCP Azure!"
+    return "Hello World - from Crossvale on OCP Azure! " + env.get('ocp-api-secret')
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", debug=True, port=5000)
