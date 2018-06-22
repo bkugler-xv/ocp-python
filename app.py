f5f86f0765fb3960589ad44c9fe207d0b1bb4bf6
Brett Kugler <bkugler@gdt-labs.local>
2018-06-22 11:33:34 -0500

    test OCP API

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from os import environ as env
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World - from Crossvale on OCP Azure! " + env.get('password')
+	apikey = env.get('password')
+    url = 'https://masterdnsx6zh27iw534mg.eastus.cloudapp.azure.com/oapi/v1'
+    headers = {'Authorization': 'Bearer '+str(apikey)}
+    resp = requests.get(url, headers=headers, verify=False)	
+    return "Hello World - from Crossvale on OCP Azure! \n " + resp.json()
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", debug=True, port=5000)
