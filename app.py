b41e65f7141a265e55269f869b95bdb2b5adb5b0
Brett Kugler <bkugler@gdt-labs.local>
2018-06-22 10:20:43 -0500

    start from within

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World - from Crossvale on OCP Azure!"
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", debug=True, port=8080)
